3fc65a1dbec8a7de942ef7e12f78b47eb1120f96
Frankie Bagnardi <fbagnardi@gmail.com>
2013-08-15 06:37:33 -0700

    fixed positioning bug for right/bottom part of screen

--- knockout-righty.js
+++ knockout-righty.js
@@ -39,10 +39,28 @@
                 if (menuElements[menu]) {
                     menuElements[menu].style.display = "none";
                 }
+                if (window.removeEventListener) {
+                    window.removeEventListener("mousemove", MouseMoveHandler);
+                }
+            }
+
+            bindEvent("click", document.body, hideMenu);
+
+            // hides the menu when we move more than 20 pixels away from one edge
+            function MouseMoveHandler(event) {
+                var ex = event.pageX, ey = event.pageY,
+                    t = 20, // pixel threshold
+                    m = menuElements[menu],
+                    mLeft = parseInt(m.style.left), mRight = mLeft + m.width,
+                    mTop = parseInt(m.style.top), mBottom = mTop + m.height;
+
+                // checks if we're > t outside of the menu bounds
+                ( (ex < mLeft - t) || (ex > mRight + t) || (ey < mTop - t) || (ey > mBottom + t) ) && hideMenu();
+
             }
 
             function showMenu(event) {
-                var menuElement = menuElements[menu], offsetY = 0, offsetX = 0, scrollTop;
+                var menuElement = menuElements[menu], offsetY = 0, offsetX = 0, scrollTop, size;
 
                 // Call the menu show event handler (which defaults to a noop)
                 menuShow[menu].call(element, element);
@@ -51,28 +69,36 @@
 
                 scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
+                menuElement.style.position = "absolute";
+                menuElement.style.display = "block";
+
+                size = window.getComputedStyle ? window.getComputedStyle(menuElement) : {width: 0, height: 0}
+
                 if (event.pageY > scrollTop + (document.documentElement.clientHeight * 0.5)) {
-                    offsetY = -menuElement.height;
+                    offsetY -= parseInt(size.height);
+                    console.log(size.height);
                 }
 
                 // if we'r on the right side of the page, push it over
                 if (event.pageX > document.documentElement.clientWidth * 0.5) {
-                    offsetX = -menuElement.width;
+                    offsetX -= parseInt(size.width);
                 }
 
-                menuElement.style.position = "absolute";
-                menuElement.style.display = "block";
                 menuElement.style.top = offsetY + event.pageY + "px";
-                menuElement.style.left = offsetX + event.pageY + "px";
+                menuElement.style.left = offsetX + event.pageX + "px";
             }
 
             if (menu) {
                 function handleContext(event) {
                     // The isDescendant check allows bind to a node for all of its children
                     if (event.target === element || isDescendant(element, event.target) && menuElements[menu]) {
-                        showMenu(menu);
-                        bindEvent("click", document.body, hideMenu);
+                        showMenu(event);
                         event.preventDefault();
+
+                        // ehh, IE8 users don't need this (it would be insanely complicated)
+                        if (window.addEventListener) {
+                            window.addEventListener("mousemove", MouseMoveHandler);
+                        }
                     }
                 }
 
@@ -85,10 +111,11 @@
     ko.bindingHandlers.menuName = {
         init: function (element, valueAccessor, allBindingsAccessor) {
             var menu = ko.unwrap(valueAccessor()),
-            allBindings = allBindingsAccessor();
+                allBindings = allBindingsAccessor();
 
             // event handler for the menu being shown
-            menuShow[menu] = allBindings.menuShow || function(){};
+            menuShow[menu] = allBindings.menuShow || function () {
+            };
 
             if (menu) {
                 menuElements[menu] = element;
